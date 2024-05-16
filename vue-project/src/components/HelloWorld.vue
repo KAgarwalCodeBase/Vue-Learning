f98dc61f115c2ebe5be3e9ba25154ff2ac4d6eb4
KAgarwalCodeBase1 <kaushalagarwal1997@gmail.com>
2024-05-16 16:31:03 -0400

    creating first component

---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<!-- JS -->
+<script setup>
+    console.log('hellow world');
+</script>
+
+<!-- HTML -->
+<template>
+    <h1>Hellow World!!</h1>
+</template>
+
+<!-- CSS -->
+<style>
+h1 {
+    color: red;
+}
+</style>
