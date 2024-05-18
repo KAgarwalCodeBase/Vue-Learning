5a1ff7bcacdc151c6c631638bc1e19c2e128f118
KAgarwalCodeBase1 <kaushalagarwal1997@gmail.com>
2024-05-18 15:22:07 -0400

    interpolation

---
+++ vue-project-interpolation/src/components/MyComponent.vue
@@ -0,0 +1,12 @@
+<script setup>
+var firstname = "Kaushal";
+var lastname = "Agarwal";
+function add (x,y){
+    return x+y;
+}
+
+</script>
+<template>
+<h1>{{firstname +" "+ lastname }}</h1>
+<h2>Add Two numbers: {{add(2,4)}}</h2>
+</template>
