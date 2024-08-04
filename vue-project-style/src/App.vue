5f8718b053f17916369cc93e5e008063f0e448cd
KAgarwalCodeBase1 <kaushalagarwal1997@gmail.com>
2024-08-04 16:34:07 -0400

    adding module style

--- vue-project-style/src/App.vue
+++ vue-project-style/src/App.vue
@@ -2,10 +2,13 @@
 import GlobalStyle from './components/GlobalStyle.vue';
 import LocalStyle from './components/LocalStyle.vue';
 import CombinedStyle  from './components/CombinedStyle.vue';
+import ModuleStyle from './components/ModuleStyle.vue';
 </script>
+
 
 <template>
 <GlobalStyle/>
 <LocalStyle/>
 <CombinedStyle/>
+<ModuleStyle/>
 </template>
---
+++ vue-project-style/src/components/ModuleStyle.vue
@@ -0,0 +1,16 @@
+<template>
+<h1 v-bind:class="$style['m-class']"> This is module example.</h1>
+<h1 :class="$style['l-class']"> This is another module example.</h1>
+</template>
+
+
+<style module>
+.m-class {
+    background: firebrick;
+    color: white;
+}
+.l-class {
+    background: green;
+    color: white;
+}
+</style>
