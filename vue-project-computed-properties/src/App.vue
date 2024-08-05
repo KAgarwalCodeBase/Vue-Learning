0f66fb46e1a36d87300c1ead42d65b8039beaa7c
KAgarwalCodeBase1 <kaushalagarwal1997@gmail.com>
2024-08-05 18:32:01 -0400

    computed properties

---
+++ vue-project-computed-properties/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import MyComputedPropertiesExample from './components/MyComputedPropertiesExample.vue';
+</script>
+
+<template>
+  <MyComputedPropertiesExample/>
+</template>
---
+++ vue-project-computed-properties/src/components/MyComputedPropertiesExample.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref, computed } from 'vue';
+let count = ref(0);
+let square = computed(()=>count.value**2)
+</script>
+
+<template>
+<h1>Count : {{count}}</h1>
+<h1>Square: {{square}}</h1>
+<button @:click="count++">Increment</button>
+</template>
